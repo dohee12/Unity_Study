8a7c25f05d343ad0aec4db55243fff7195784752
dohee12 <happyddo42@gmail.com>
2025-03-27 14:14:46 +0900

    feat: CharacterController를 사용한 Player 이동 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 // - Input Key
 // - Direction (앞/뒤/좌/우)
 // - Move Speed
+// 1-a. CharacterController 를 통해서 Player 움직인다
+//  - Component : CharacterController
+//  - 중력 적용
+//      ㄴ gravity : 중력값 -9.81
+//      ㄴ yVelocity : Player가 적용되는 중력값
+//  - Jump 기능
+//      ㄴ JumpPower
+//      ㄴ 점프가 가능한 상태인지? (땅을 밟고 있는지 여부) 
+//      ㄴ n단 점프
+//         - jumpCount : 현재 점프 룃수
+//         - maxCount : 최대 점프 횟수
 // 2. player를 회전시킨다
 // - 방향 (좌/우, 상/하)
 // - 누적 방향 값
@@ -22,34 +33,85 @@ public class Player : MonoBehaviour
     // 제한 시야각
     public float limitAngle = 80.0f;
 
-    // Rigidbody Component
-    public Rigidbody rb;
+    // Characher Controller
+    private CharacterController cc;
+
+    // 중력 상수값
+    private float gravity = -9.81f;
+
+    // Player가 적용되는 중력값
+    private float yVelocity;
+
+    // player의 JumpPower
+    public float JumpPower = 3.0f;
+
+    // Player의 점프 가능 횟수
+    public int jumpCount = 0;
+
+    // Player의 점프 가능 최대 횟수
+    public int maxJumpCount = 2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() // 한번만 실행행
     {
-
+        // Play 시에, cc에 내가 가진 CharacterController 컴포넌트 할당
+        cc = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update() // 게임 오프젝트가 죽을 때까지
     {
-        Movement();
+        //Movement();
 
         RotateHorizontal();
 
         RotateVertical();
 
-        Jump();
+        CCMovemnet();
+
     }
 
-    // Player Jump
-    void Jump() {
-        // 점프키를 누르면
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            // Y축 방향으로 점프파워만큼 점프!
-            rb.AddForce(Vector3.up * 5.0f, ForceMode.Impulse);
+    // CharacterController 사용한 Player 이동
+    void CCMovemnet() {
+        // 1. Input 이동 방향 받아오기
+        // a. 좌/우 방향
+        float h = Input.GetAxis("Horizontal");
+        // b. 앞/뒤 방향
+        float v = Input.GetAxis("Vertical");
+        // 2. 방향 만들기 (받아온 이동 값으로부터)
+        Vector3 dir = new Vector3(h,0,v);
+        // 문제 : 내가 바라보는 방향을 기준으로 움직이지 않음
+        // 해결 : Camera가 바라보는 방향으로 기준을 방향변경
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        // -- Player의 점프 ----
+        // ** 도전 과제 : n단 점프 **
+        // a,b : maxCount, jumpcount 
+        // 1. 땅에 닿아있는 경우
+        if(cc.isGrounded) {
+            //  => 점프 횟수 초기화(Reset)
+            jumpCount = maxJumpCount;
+            //  => yVelocity 중력값 초기화
+            yVelocity = 0f;
         }
+        // 2. 점프 버튼을 누른 경우,점프 횟수가 0 이상인 경우에만 점프 가능
+        if(jumpCount > 0 && Input.GetButtonDown("Jump")) {
+            // => 점프 횟수 -1씩
+            jumpCount--;
+            // => 점프 파워만큼 점프
+            yVelocity = JumpPower;
+        }
+        
+        // 문제 : Player가 상/하로 움직임
+        // 해결 : 중력적용을 통해서 상/하 움직임 제한
+        // 공식 : f(힘) = 질량(m) * 가속도(a)
+        // a. 중력 가속도 구하기
+        yVelocity += gravity * Time.deltaTime;
+        // b. Player의 방향에 적용
+        dir.y = yVelocity;
+
+        // 3. Player 움직이기 with CharacterController
+        cc.Move(dir * moveSpeed * Time.deltaTime);
     }
 
     // player를 상/하 회전
@@ -96,7 +158,7 @@ public class Player : MonoBehaviour
         //  - 좌/우 방향
         float h = Input.GetAxis("Horizontal");
         // 2. 어느 방향으로?
-        Vector3 direction = new Vector3(1, 2, 3);
+        Vector3 direction = new Vector3(h,0,v);
         // 1. 어느정도의 빠르기로?
         transform.Translate(direction * moveSpeed * Time.deltaTime);
         // Goal : 이동시킨다
